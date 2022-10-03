52562c2dce871e594e742659a16dc1d9e50c83fb
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-03 16:23:04 -0300

    feat: add set and create methods to context

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -1,44 +1,18 @@
+import { Transaction } from '@prisma/client';
 import create from 'zustand';
-
-// import { api } from '../services/api';
-
-export interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  category: string;
-  amount: number;
-  createdAt: string;
-}
-
-type CreateTransactionData = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsContext {
   transactions: Transaction[];
-  createTransaction: (data: CreateTransactionData) => Promise<void>;
-  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (transaction: Transaction) => void;
+  setTransactions: (transactions: Transaction[]) => void;
 }
 
 export const useTransaction = create<TransactionsContext>((set, get) => ({
   transactions: [],
-  fetchTransactions: async query => {
-    // const response = await api.get<Transaction[]>('/transactions', {
-    //   params: {
-    //     _sort: 'createdAt',
-    //     _order: 'desc',
-    //     q: query
-    //   }
-    // });
-    // const { data } = response;
-    // set({ transactions: data });
+  setTransactions: transactions => {
+    set({ transactions });
   },
-  createTransaction: async data => {
-    // const response = await api.post('/transactions', {
-    //   ...data,
-    //   createdAt: new Date()
-    // });
-    // if (response.status === 201) {
-    //   set(state => ({ transactions: [response.data, ...state.transactions] }));
-    // }
+  createTransaction: transaction => {
+    set(state => ({ transactions: [transaction, ...state.transactions] }));
   }
 }));
