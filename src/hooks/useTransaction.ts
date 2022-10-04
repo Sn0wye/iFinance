f338e6cfff00eee3ec535fef8a980abd0a6f3055
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-04 08:52:59 -0300

    feat: add filtered transactions to context

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -3,16 +3,33 @@ import create from 'zustand';
 
 interface TransactionsContext {
   transactions: Transaction[];
+  filteredTransactions: Transaction[];
   createTransaction: (transaction: Transaction) => void;
   setTransactions: (transactions: Transaction[]) => void;
+  filterByQuery: (q: string) => void;
 }
 
 export const useTransaction = create<TransactionsContext>((set, get) => ({
   transactions: [],
+  filteredTransactions: [],
   setTransactions: transactions => {
-    set({ transactions });
+    set({ transactions, filteredTransactions: transactions });
+  },
+  filterByQuery: q => {
+    if (q.trim() === '') {
+      set(state => ({ filteredTransactions: state.transactions }));
+      return;
+    }
+    set(state => ({
+      filteredTransactions: state.transactions.filter(transaction =>
+        transaction.description.toLowerCase().includes(q.toLowerCase())
+      )
+    }));
   },
   createTransaction: transaction => {
-    set(state => ({ transactions: [transaction, ...state.transactions] }));
+    set(state => ({
+      transactions: [transaction, ...state.transactions],
+      filteredTransactions: [transaction, ...state.transactions]
+    }));
   }
 }));
