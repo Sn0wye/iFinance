bba1b09e3ea38136c58f37259a449e3cf287c16e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-05 10:07:33 -0300

    style: add scale keyframes

---
+++ src/styles/keyframes/scale.ts
@@ -0,0 +1,21 @@
+import { keyframes } from 'styled-components';
+
+export const scaleUp = keyframes`
+  from {
+    scale: 0.95;
+
+  }
+  to {
+    scale: 1;
+  }
+`;
+
+export const scaleDown = keyframes`
+  from {
+    scale: 1;
+
+  }
+  to {
+    scale: 0.95;
+  }
+`;
