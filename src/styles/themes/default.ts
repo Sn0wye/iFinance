db2f474f6ca8c237653b515c3881ba4dbb280a9f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-07 10:28:39 -0300

    style: add brand colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -14,6 +14,17 @@ export const defaultTheme = {
   'green-500': '#00875F',
   'green-700': '#015F43',
 
+  'brand-50': '#59e092',
+  'brand-100': '#4fd688',
+  'brand-200': '#45cc7e',
+  'brand-300': '#3bc274',
+  'brand-400': '#31b86a',
+  'brand-500': '#27ae60',
+  'brand-600': '#1da456',
+  'brand-700': '#139a4c',
+  'brand-800': '#099042',
+  'brand-900': '#008638',
+
   'red-300': '#F75A68',
   'red-500': '#AB222E',
   'red-700': '#7A1921'
