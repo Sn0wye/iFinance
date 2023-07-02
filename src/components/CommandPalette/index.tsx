fca8a0e5f5244c7c9e60b76de5778ef8b65ec4c5
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-07-02 16:30:36 -0300

    feat: ill do that later

--- src/components/CommandPalette/index.tsx
+++ src/components/CommandPalette/index.tsx
@@ -1,71 +1,71 @@
-import { Action, ActionImpl, KBarPortal, KBarResults, useMatches } from 'kbar';
-import { Fragment } from 'react';
+// import { Action, ActionImpl, KBarPortal, KBarResults, useMatches } from 'kbar';
+// import { Fragment } from 'react';
 
-import {
-  Animator,
-  Kbd,
-  Positioner,
-  ResultItemStyle,
-  Search,
-  Section,
-  Shortcut
-} from './styles';
+// import {
+//   Animator,
+//   Kbd,
+//   Positioner,
+//   ResultItemStyle,
+//   Search,
+//   Section,
+//   Shortcut
+// } from './styles';
 
-export const CommandPalette = () => {
-  return (
-    <KBarPortal>
-      <Positioner>
-        <Animator>
-          <Search placeholder='Type a command or search…' />
-          <Results />
-        </Animator>
-      </Positioner>
-    </KBarPortal>
-  );
-};
+// export const CommandPalette = () => {
+//   return (
+//     <KBarPortal>
+//       <Positioner>
+//         <Animator>
+//           <Search placeholder='Type a command or search…' />
+//           <Results />
+//         </Animator>
+//       </Positioner>
+//     </KBarPortal>
+//   );
+// };
 
-interface RenderParams {
-  item: ActionImpl | string;
-  active: boolean;
-}
+// interface RenderParams {
+//   item: ActionImpl | string;
+//   active: boolean;
+// }
 
-const Results = () => {
-  const { results } = useMatches();
+// const Results = () => {
+//   const { results } = useMatches();
 
-  return (
-    <KBarResults
-      items={results}
-      onRender={({ item, active }: RenderParams) =>
-        typeof item === 'string' ? (
-          <Section>{item}</Section>
-        ) : (
-          <ResultItem action={item} active={active} />
-        )
-      }
-    />
-  );
-};
+//   return (
+//     <KBarResults
+//       items={results}
+//       onRender={({ item, active }: RenderParams) =>
+//         typeof item === 'string' ? (
+//           <Section>{item}</Section>
+//         ) : (
+//           <ResultItem action={item} active={active} />
+//         )
+//       }
+//     />
+//   );
+// };
 
-interface ResultItemProps {
-  action: Action;
-  active: boolean;
-}
+// interface ResultItemProps {
+//   action: Action;
+//   active: boolean;
+// }
 
-const ResultItem = ({ action, active }: ResultItemProps) => {
-  const { icon = <Fragment />, name, shortcut: shortcuts } = action;
-  return (
-    <ResultItemStyle active={active}>
-      {icon}
-      <span>{name}</span>
-      {shortcuts && (
-        <Shortcut aria-hidden>
-          {shortcuts.map(shortcut => (
-            <Kbd key={shortcut}>{shortcut}</Kbd>
-          ))}
-        </Shortcut>
-      )}
-    </ResultItemStyle>
-  );
-};
+// const ResultItem = ({ action, active }: ResultItemProps) => {
+//   const { icon = <Fragment />, name, shortcut: shortcuts } = action;
+//   return (
+//     <ResultItemStyle active={active}>
+//       {icon}
+//       <span>{name}</span>
+//       {shortcuts && (
+//         <Shortcut aria-hidden>
+//           {shortcuts.map(shortcut => (
+//             <Kbd key={shortcut}>{shortcut}</Kbd>
+//           ))}
+//         </Shortcut>
+//       )}
+//     </ResultItemStyle>
+//   );
+// };
 
-ResultItem.displayName = 'ResultItem';
+// ResultItem.displayName = 'ResultItem';
--- src/components/CommandPalette/styles.ts
+++ src/components/CommandPalette/styles.ts
@@ -1,112 +1,112 @@
-import { KBarAnimator, KBarPositioner, KBarSearch } from 'kbar';
-import styled, { css } from 'styled-components';
+// import { KBarAnimator, KBarPositioner, KBarSearch } from 'kbar';
+// import styled, { css, keyframes } from 'styled-components';
 
-export const shake = keyframes`
-  0%, 100% {
-    transform: 'rotate(0deg)'
-  }
-  50% {
-    transform: 'rotate(18deg)'
-  }
-`;
+// export const shake = keyframes`
+//   0%, 100% {
+//     transform: 'rotate(0deg)'
+//   }
+//   50% {
+//     transform: 'rotate(18deg)'
+//   }
+// `;
 
-export const Positioner = styled(KBarPositioner)`
-  position: fixed;
-  inset: 0;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  width: 100%;
-  padding: 25vh 1rem 1rem;
-  overflow-y: auto;
-  background: rgba(0, 0, 0, 0.75);
-  box-sizing: border-box;
-`;
+// export const Positioner = styled(KBarPositioner)`
+//   position: fixed;
+//   inset: 0;
+//   display: flex;
+//   align-items: flex-start;
+//   justify-content: center;
+//   width: 100%;
+//   padding: 25vh 1rem 1rem;
+//   overflow-y: auto;
+//   background: rgba(0, 0, 0, 0.75);
+//   box-sizing: border-box;
+// `;
 
-export const Search = styled(KBarSearch)`
-  width: 100%;
-  background: transparent;
-  padding: 1rem;
-  font-size: 1rem;
-  color: ${({ theme }) => theme['gray-100']};
+// export const Search = styled(KBarSearch)`
+//   width: 100%;
+//   background: transparent;
+//   padding: 1rem;
+//   font-size: 1rem;
+//   color: ${({ theme }) => theme['gray-100']};
 
-  ::placeholder {
-    color: ${({ theme }) => theme['gray-300']};
-  }
+//   ::placeholder {
+//     color: ${({ theme }) => theme['gray-300']};
+//   }
 
-  :focus {
-    box-shadow: none;
-  }
-`;
+//   :focus {
+//     box-shadow: none;
+//   }
+// `;
 
-export const Animator = styled(KBarAnimator)`
-  max-width: 600px;
-  width: 100%;
-  overflow: hidden;
-  background: ${({ theme }) => theme['gray-800']};
-  border-radius: 6px;
+// export const Animator = styled(KBarAnimator)`
+//   max-width: 600px;
+//   width: 100%;
+//   overflow: hidden;
+//   background: ${({ theme }) => theme['gray-800']};
+//   border-radius: 6px;
 
-  & > div ::-webkit-scrollbar {
-    display: none;
-  }
-`;
+//   & > div ::-webkit-scrollbar {
+//     display: none;
+//   }
+// `;
 
-export const Hr = styled.hr`
-  border-top: 1px solid ${({ theme }) => theme['gray-500']};
-  margin-top: 0.75rem;
-  margin-bottom: 0.75rem;
-`;
+// export const Hr = styled.hr`
+//   border-top: 1px solid ${({ theme }) => theme['gray-500']};
+//   margin-top: 0.75rem;
+//   margin-bottom: 0.75rem;
+// `;
 
-export const Section = styled.div`
-  margin: 0 0.75rem;
-  padding: 0.75rem;
-  font-size: 0.875rem;
-  line-height: 1.125rem;
-  font-weight: 600;
-  color: ${({ theme }) => theme['gray-300']};
-`;
+// export const Section = styled.div`
+//   margin: 0 0.75rem;
+//   padding: 0.75rem;
+//   font-size: 0.875rem;
+//   line-height: 1.125rem;
+//   font-weight: 600;
+//   color: ${({ theme }) => theme['gray-300']};
+// `;
 
-interface ResultItemStyleProps {
-  active: boolean;
-}
+// interface ResultItemStyleProps {
+//   active: boolean;
+// }
 
-export const ResultItemStyle = styled.div<ResultItemStyleProps>`
-  display: flex;
-  align-items: center;
-  gap: 0.75rem;
-  padding: 0.75rem;
-  color: ${({ theme }) => theme['gray-400']};
-  border-left: 2px solid transparent;
+// export const ResultItemStyle = styled.div<ResultItemStyleProps>`
+//   display: flex;
+//   align-items: center;
+//   gap: 0.75rem;
+//   padding: 0.75rem;
+//   color: ${({ theme }) => theme['gray-400']};
+//   border-left: 2px solid transparent;
 
-  ${({ active }) =>
-    active &&
-    css`
-      cursor: pointer;
-      background: ${({ theme }) => theme['gray-900']};
-      color: ${({ theme }) => theme['gray-100']};
-      border-left: 2px solid ${({ theme }) => theme['brand-500']};
+//   ${({ active }) =>
+//     active &&
+//     css`
+//       cursor: pointer;
+//       background: ${({ theme }) => theme['gray-900']};
+//       color: ${({ theme }) => theme['gray-100']};
+//       border-left: 2px solid ${({ theme }) => theme['brand-500']};
 
-      & svg {
-        animation: ${shake} 0.2s ease-in-out;
-      }
-    `};
-`;
+//       & svg {
+//         animation: ${shake} 0.2s ease-in-out;
+//       }
+//     `};
+// `;
 
-export const Shortcut = styled('div')`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 0.25rem;
-  margin-left: auto;
-`;
+// export const Shortcut = styled('div')`
+//   display: grid;
+//   grid-auto-flow: column;
+//   gap: 0.25rem;
+//   margin-left: auto;
+// `;
 
-export const Kbd = styled.kbd`
-  background: rgba(255, 255, 255, 0.1);
-  color: #8f9ba8;
-  padding: 0.25rem 0.5rem;
-  text-transform: uppercase;
-  border-radius: 4px;
-  transition: background-color 0.2s ease-in-out;
-  font-family: 'Roboto';
-  font-size: 0.875rem;
-  font-weight: 500;
-`;
+// export const Kbd = styled.kbd`
+//   background: rgba(255, 255, 255, 0.1);
+//   color: #8f9ba8;
+//   padding: 0.25rem 0.5rem;
+//   text-transform: uppercase;
+//   border-radius: 4px;
+//   transition: background-color 0.2s ease-in-out;
+//   font-family: 'Roboto';
+//   font-size: 0.875rem;
+//   font-weight: 500;
+// `;
