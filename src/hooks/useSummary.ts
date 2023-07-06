4e86afc1dc2af577afe8f19e728f295650bc8da4
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-07-06 16:58:54 -0300

    fix: use summary hook

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -4,7 +4,12 @@ import { api } from '~/utils/api';
 export const useSummary = () => {
   const { data: transactions } = api.transactions.getAll.useQuery();
 
-  if (!transactions) return;
+  if (!transactions)
+    return {
+      income: 0,
+      expense: 0,
+      total: 0
+    };
 
   const summary = useMemo(() => {
     return transactions.reduce(
